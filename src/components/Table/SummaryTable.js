9392c2e2077eb1610bfa0bdcdb7efbdf165dceb3
maggie-mar <magdalena.martin2024@mauihigh.org>
2022-02-19 14:42:22 -1000

    Got teams from blue alliance using fetch

--- src/components/Table/SummaryTable.js
+++ src/components/Table/SummaryTable.js
@@ -1,13 +1,44 @@
 import React from "react";
-import { useTable, useSortBy } from "react-table";
+import { useTable, useSortBy, useExpanded } from "react-table";
 import SampleData from "./Data";
 
 const SummaryTable = (props) => {
 
     const data = SampleData();
 
+    const teams = () => {
+
+        fetch('https://www.thebluealliance.com/api/v3/event/2022hiho/teams', {mode: "cors", headers: {'X-TBA-Auth-Key': 'B9xCtlRyJheUGvzJShpl1QkOor35UTPO8GUtpn7Uq9xB5aJQL44yNzXnTZBHpWXz'}})
+            .then(response => response.json())
+            .catch(err => console.log(err))
+            .then(data => console.log(data));
+
+    }
+    teams()
+
     const columns = React.useMemo(
         () => [
+            {
+                id: 'expander',
+                Header: ({ getToggleAllRowsExpandedProps, isAllRowsExpanded }) => (
+                    <span {...getToggleAllRowsExpandedProps()}>
+                        {isAllRowsExpanded? '':''}
+                    </span>
+                ),
+                Cell: ({ row }) =>
+                row.canExpand ? (
+                    <span
+                    {...row.getToggleAllRowsExpandedProps({
+                        style: {
+                            paddingleft: `${row.depth * 2}rem`,
+                        }
+
+                    })}
+                    >
+                        {row.isExpanded ? '':''}
+                    </span>
+                ) : null,
+            },
             {
                 Header: 'Team #',
                 accessor: 'TeamNumber',
@@ -44,7 +75,7 @@ const SummaryTable = (props) => {
         []
     )
 
-    const tableInstance = useTable({ columns, data }, useSortBy);
+    const tableInstance = useTable({ columns, data }, useSortBy, useExpanded);
 
     const {
         getTableProps,
@@ -52,9 +83,9 @@ const SummaryTable = (props) => {
         headerGroups,
         rows,
         prepareRow,
-    } = tableInstance
-
-
+        state: { expanded },
+    } = tableInstance 
+    
 
     return (
         <div>
@@ -102,9 +133,9 @@ const SummaryTable = (props) => {
                                                         padding: '10px',
                                                         border: 'solid 1px black',
                                                         textAlign: 'center',
-                                                    }}
-                                                >
+                                                    }}>
                                                     {cell.render('Cell')}
+
                                                 </td>
                                             )
                                         }
@@ -118,6 +149,7 @@ const SummaryTable = (props) => {
                 </tbody>
 
             </table>
+
         </div>
     )
 
