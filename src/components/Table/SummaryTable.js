cc9c2146f77fa9ae73d663fd705b21a14a7bb426
alessandraGudoy <alessandra.gudoy2023@mauihigh.org>
2022-03-19 08:46:43 -1000

    checking if teams from matches is fetched using san diego regional

--- src/components/Table/SummaryTable.js
+++ src/components/Table/SummaryTable.js
@@ -119,9 +119,9 @@ const SummaryTable = () => {
     }
 
     const getTeams = async () => {                              // Get list of teams from the Blue Alliance
-        const key = await api.getRegional();
-        console.log(`https://www.thebluealliance.com/api/v3/event/${key}/teams`);
-        return await fetch(`https://www.thebluealliance.com/api/v3/event/${key}/teams`, { mode: "cors", headers: { 'x-tba-auth-key': await api.getBlueAllianceAuthKey() } })
+        /*const key = await api.getRegional();
+        console.log(`https://www.thebluealliance.com/api/v3/event/${key}/teams`);*/
+        return await fetch(`https://www.thebluealliance.com/api/v3/event/2022casd/teams`, { mode: "cors", headers: { 'x-tba-auth-key': await api.getBlueAllianceAuthKey() } })
             .catch(err => console.log(err))
             .then(response => response.json())
             .then(data => {
