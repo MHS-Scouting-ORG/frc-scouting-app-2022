1b848da71c4311dc783f52affb8ca7cb80a3f0be
alessandraGudoy <alessandra.gudoy2023@mauihigh.org>
2022-04-21 18:27:41 -1000

    filtering out qual match 0

--- src/components/Table/Summary.js
+++ src/components/Table/Summary.js
@@ -31,7 +31,7 @@ const Summary = () => {
 
     useEffect(() => setAverages(teamNumbers.map(team => {   // Calculate averages for each team
         //const tempStats = apiData.filter(x => parseInt(x.TeamId) === team.TeamNumber).filter(x => parseInt(x.MatchId.substring(x.MatchId.indexOf('_')+2)) !== 0);
-        const teamStats = apiData.filter(x => parseInt(x.TeamId) === team.TeamNumber).filter(x => parseInt(x.MatchId.substring(x.MatchId.indexOf('_')+2)) !== 0 || x.MatchId.indexOf('qm0') === -1).filter(x => x.MatchId.substring(0, 7) === '2022hop');
+        const teamStats = apiData.filter(x => parseInt(x.TeamId) === team.TeamNumber).filter(x => parseInt(x.MatchId.substring(x.MatchId.indexOf('_')+2)) !== 0).filter(x => x.MatchId.indexOf('qm0') === -1).filter(x => x.MatchId.substring(0, 7) === '2022hop');
         //    const teamStats = apiData.filter(x => parseInt(x.TeamId) === team.TeamNumber).filter(x => x.SummaryComment === undefined);
         const summaryComment = apiData.filter(x => parseInt(x.TeamId) === team.TeamNumber).filter(x => parseInt(x.MatchId.substring(x.MatchId.indexOf('_')+2)) === 0);
         //    const summaryComment = apiData.filter(x => parseInt(x.TeamId) === team.TeamNumber).filter(x => x.SummaryComment !== undefined);
